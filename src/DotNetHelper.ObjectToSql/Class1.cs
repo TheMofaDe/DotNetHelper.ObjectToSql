e23aac42948acc96f25163f95bf07d9f5b257676
Joseph McNeal Jr <josephmcnealjr@mofade.com>
2019-07-04 10:10:56 -0500

    working on docs

---
+++ src/DotNetHelper.ObjectToSql/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetHelper.ObjectToSql
+{
+     class Class1
+     {
+         public string a;
+        public Class1()
+        {
+
+        }
+    }
+}
